8893e67e60a222c47bfb240b443542e45ccbe196
codawi <codawi358@gmail.com>
2022-05-30 09:05:11 +0900

    add:lighthouse

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('work.index');
+        return Inertia::render('work.index');
     }
 }
