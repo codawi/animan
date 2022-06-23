412fe2b89f2bb6cb35ec38eae3b5e5ccd67bf09a
codawi <codawi358@gmail.com>
2022-06-23 13:23:36 +0900

    add:トップ画面作品表示 #5

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Work;
+use Illuminate\Queue\Events\WorkerStopping;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Work/index');
+        $anime_works = Work::where('id', '4')->first();
+        return Inertia::render('Work/index',
+    ['animeWorks' => $anime_works]);
         
     }
 }
