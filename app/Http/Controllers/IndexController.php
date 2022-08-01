38269e049d5bd90c9b488eb9f68d7c2915de37b6
codawi <codawi358@gmail.com>
2022-08-01 15:50:13 +0900

    add:カウント数順に作品ソート #34

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TweetCount;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Work;
@@ -13,6 +14,8 @@ class IndexController extends Controller
     {
         $anime_works = Work::where('category', 'anime')->take(10)->get();
         $comic_works = Work::where('category', 'comic')->take(10)->get();
+        $anime_ranking = TweetCount::orderBy('daily_tweet', 'desc')->with('work')->take(10)->get()->toArray();
+        dd($anime_ranking);
         
         return Inertia::render('Work/index',
     ['animeWorks' => $anime_works, 'comicWorks' => $comic_works]);
