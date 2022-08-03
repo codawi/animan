f845a2cff3b4e80d7141f11e125e47b27ae3c9af
codawi <codawi358@gmail.com>
2022-08-03 13:30:36 +0900

    add:トップ画面にランキング形式で一覧表示 #34

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,12 +14,14 @@ class IndexController extends Controller
     {
         //アニメランキング
         $anime_ranking = Work::with('count')->where('category', 'anime')->get();
-        $anime_ranking = $anime_ranking->sortByDesc('count.daily_tweet')->toArray();
+        $anime_ranking = $anime_ranking->sortByDesc('count.daily_tweet')->take(10)->toArray();
 
         //漫画ランキング
         $comic_ranking = Work::with('count')->where('category', 'comic')->get();
-        $comic_ranking = $comic_ranking->sortByDesc('count.daily_tweet')->toArray();
+        $comic_ranking = $comic_ranking->sortByDesc('count.daily_tweet')->take(10)->toArray();
 
+        // dd($comic_ranking);
+        
         return Inertia::render('Work/index',
     ['animeWorks' => $anime_ranking, 'comicWorks' => $comic_ranking]);
         
--- resources/js/Pages/Work/index.vue
+++ resources/js/Pages/Work/index.vue
@@ -2,7 +2,7 @@
 <div class="md:flex">
 <!-- アニメ作品表示 -->
 <section class="text-gray-600 body-font md:ml-16">
-  <div  v-for="animeWork in animeWorks" :key="animeWork.anime" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+  <div  v-for="animeWork in animeWorks" :key="animeWork" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
       <div v-if="animeWork.image !== null">
        <img :src=" animeWork.image ">
@@ -12,9 +12,11 @@
           </div>
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">タイトル
+      <h1 v-text="animeWork.title" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
       </h1>
-      <p class="mb-8 leading-relaxed">Copyright</p>
+      <div v-if="animeWork.copyright !== null">
+      <p v-text="animeWork.copyright" class="mb-8 leading-relaxed"></p>
+      </div>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">詳細</button>
         <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">配信サイト</button>
@@ -24,8 +26,8 @@
 </section>
 
 <!-- 漫画作品表示 -->
-<section class="text-gray-600 body-font justify-end md:mr-16">
-  <div  v-for="comicWork in comicWorks" :key="comicWork.comic" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+<section class="text-gray-600 body-font md:ml-16">
+  <div  v-for="comicWork in comicWorks" :key="comicWork" class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
       <div v-if="comicWork.image !== null">
        <img :src=" comicWork.image ">
@@ -35,9 +37,11 @@
           </div>
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">タイトル
+      <h1 v-text="comicWork.title" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
       </h1>
-      <p class="mb-8 leading-relaxed">Copyright</p>
+      <div v-if="comicWork.copyright !== null">
+      <p v-text="comicWork.copyright" class="mb-8 leading-relaxed"></p>
+      </div>
       <div class="flex justify-center">
         <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">詳細</button>
         <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">配信サイト</button>
@@ -53,7 +57,6 @@
     import Footer from "@/Components/Footer"
 
     export default {
-        name: 'app',
         components: { Header, Footer },
         props:{
           animeWorks: {
@@ -65,80 +68,3 @@
     },
     }
 </script>
-
-<!-- <template>
-    <div id="app">
-        <Header/>
-        <section class="text-gray-600 body-font">
-          アニメ作品表示
-  <div class="container flex justify-center mx-auto">
-    <div class="flex-col max-w-2xl mb-10 text-center">
-      <div v-for="animeWork in animeWorks" :key="animeWork.anime" class="my-36 px-4">
-        <div class="rounded-lg overflow-hidden">
-          <div v-if="animeWork.image !== null">
-           <img :src=" animeWork.image ">
-          </div>
-          <div v-else class="flex justify-center">
-            <img :src="'/img/no_image_logo.png'">
-          </div>
-        </div>
-        <h2 class="title-font text-2xl font-medium text-gray-900 mt-6 mb-3">{{ animeWork.title }}</h2>
-        <p class="leading-relaxed text-xs font-thin" >{{ animeWork.copyright }}</p>
-        <button class="flex mx-auto mt-4 text-white bg-yellow-500 border-0 py-4 px-8 focus:outline-none hover:bg-yellow-600 rounded">詳細</button>
-      </div>
-    </div>
-  </div>
-</section>
-        <Footer/>
-    </div>
-</template>
-
-<script>
-    import Header from "@/Components/Header"
-    import Footer from "@/Components/Footer"
-
-    export default {
-        name: 'app',
-        components: {Header, Footer},
-        props:{
-          animeWorks: {
-            type:Object,
-          }
-    },
-    }
-
-    
-</script>
-
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-
-    body {
-        font-family: fot-tsukuardgothic-std, sans-serif;
-        line-height: 1.5;
-        font-size: 14px;
-    }
-
-    .container {
-        width: 100%;
-        padding: 0 20px;
-        box-sizing: border-box;
-    }
-
-    @media (min-width: 768px) {
-        .container {
-            width: 700px;
-            padding: 0;
-            margin: 0 auto;
-        }
-    }
-
-    @media (min-width: 1024px) {
-        .container {
-            width: 980px;
-        }
-    }
-</style> -->
