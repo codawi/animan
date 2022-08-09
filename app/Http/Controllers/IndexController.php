7112c01a7e38046fee91ce7857ea6a77ecacab17
codawi <codawi358@gmail.com>
2022-08-09 11:52:07 +0900

    add:アニメ漫画ランキングコンポーネント化 #38

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,26 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\TweetCount;
+
 use Illuminate\Http\Request;
-use Inertia\Inertia;
-use App\Models\Work;
+
 use Illuminate\Queue\Events\WorkerStopping;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        //アニメランキング
-        $anime_ranking = Work::with('count')->where('category', 'anime')->get();
-        $anime_ranking = $anime_ranking->sortByDesc('count.daily_tweet')->take(10)->toArray();
-
-        //漫画ランキング
-        $comic_ranking = Work::with('count')->where('category', 'comic')->get();
-        $comic_ranking = $comic_ranking->sortByDesc('count.daily_tweet')->take(10)->toArray();
         
-        return Inertia::render('Index',
-    ['animeWorks' => $anime_ranking, 'comicWorks' => $comic_ranking]);
         
     }
 }
