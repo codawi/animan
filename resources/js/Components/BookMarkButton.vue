05d73013116ab129818e1faefa214b3e235c5b68
codawi <codawi358@gmail.com>
2022-10-01 14:16:00 +0900

    add:検索結果ページネーション#148

--- resources/js/Components/BookMarkButton.vue
+++ resources/js/Components/BookMarkButton.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="fa-2x my-auto px-4">
-  <div v-if="is_bookmark">
-  <button type="button">
-    <font-awesome-icon
-      icon="fa-solid fa-heart"
-      class="text-pink-500"
-      @click="bookmarkDelete(work.id)"
-    />
-  </button>
-</div>
-<div v-else>
-  <button type="button">
-    <font-awesome-icon
-      icon="fa-regular fa-heart"
-      @click="bookmarkStore(work.id)"
-    />
-  </button>
-</div>
-</div>
+    <button v-if="is_bookmark" @click="bookmarkDelete(work.id)">
+      <font-awesome-icon
+        icon="fa-solid fa-heart"
+        class="text-pink-500"
+      />
+    </button>
+    <button v-else @click="bookmarkStore(work.id)">
+      <font-awesome-icon icon="fa-regular fa-heart" />
+    </button>
+  </div>
 </template>
+
 <script>
 export default {
   props: {
     work: {
-      type: Object
+      type: Object,
     },
     is_bookmark: {
-      type: [Boolean, Object]
+      type: [Boolean, Object],
     },
   },
   methods: {
     bookmarkStore() {
       this.$inertia.post(route("bookmark.store", this.work.id), {
         preserveScroll: true,
-      })
+      });
     },
     bookmarkDelete() {
-      this.$inertia.delete(route('bookmark.delete', this.work.id), {
+      this.$inertia.delete(route("bookmark.delete", this.work.id), {
         preserveScroll: true,
-      })
-    }
+      });
+    },
   },
 };
 </script>
